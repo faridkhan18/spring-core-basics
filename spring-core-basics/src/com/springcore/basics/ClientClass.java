35ee04dbafd130539b921cdd68519327934471dc
farid <farid18khan@gmail.com>
2018-02-28 12:17:44 +0530

    ApplicationContext

--- spring-core-basics/src/com/springcore/basics/ClientClass.java
+++ spring-core-basics/src/com/springcore/basics/ClientClass.java
@@ -2,13 +2,19 @@ package com.springcore.basics;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class ClientClass {
 
 	public static void main(String[] args) {
-		BeanFactory bf = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle tri = (Triangle) bf.getBean("triangle");
+		//BeanFactory bf = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+		
+		//Triangle tri = (Triangle) bf.getBean("triangle");
+		Triangle tri = (Triangle) ac.getBean("triangle");
 		tri.draw();
 		System.out.println("Yolo");
 		
